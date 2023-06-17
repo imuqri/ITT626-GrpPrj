e4e10ae9706ea515ba748126c5d27c1a3fa66df7
Slash Hj Tapah <imuqri31@gmail.com>
2023-06-18 03:07:01 +0800

    create product model

---
+++ models/product.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose")
+
+const ProductSchema = new mongoose.Schema({
+    title: {type:String, required:true, unique:true},
+    img: {type:String , required:true},
+    color: {type:String , required:true},
+    size: {type:Number , required:true},
+    price: {type:Number , required:true},
+    
+},
+{ timestamps:true}
+);
+
+module.exports = mongoose.model("Product", ProductSchema);
