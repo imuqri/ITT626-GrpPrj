f21c14affb72b00f64347ea79558fc260a0dc1b0
Slash Hj Tapah <imuqri31@gmail.com>
2023-07-03 00:15:35 +0800

    Update user.js
    add delete and get user function, all tested working fine

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,10 @@
 const { response } = require("express");
 const User = require("../models/User");
-const { verifyToken, verifyTokenAndAuthorization } = require("./verifyToken");
+const { verifyToken, verifyTokenAndAuthorization, verifyTokenAndAdmin } = require("./verifyToken");
 
 const router = require("express").Router();
 
+//update
 router.put("/:id", verifyTokenAndAuthorization, async (req,res)=>{
     if(req.body.password) {
         req.body.password = CryptoJS.AES.encrypt(
@@ -20,6 +21,40 @@ router.put("/:id", verifyTokenAndAuthorization, async (req,res)=>{
         );
         res.status(200).json(updatedUser);
     }catch(err){res.status(500).json(err)}
+});
+
+
+//delete
+router.delete("/:id", verifyTokenAndAuthorization, async (req,res)=>{
+    try{
+        await User.findByIdAndDelete(req.params.id)
+        res.status(200).json("User has been deleted...")
+
+    }catch(err){
+        res.status(500).json(err)
+    }
+});
+
+
+//Get user
+router.get("/find/:id", verifyTokenAndAdmin, async (req,res)=>{
+    try{
+        const user = await User.findById(req.params.id);
+        const { password, ...others} = user._doc;
+        res.status(200).json(others);
+    }catch(err){
+        res.status(500).json(err)
+    }
+});
+
+//Get all user
+router.get("/", verifyTokenAndAdmin, async (req,res)=>{
+    try{
+        const users = await User.find();
+        res.status(200).json(users);
+    }catch(err){
+        res.status(500).json(err)
+    }
 });
 
 module.exports = router
