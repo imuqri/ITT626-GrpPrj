d2ebef44fc6a328796760713451a5f45852be2d7
Slash Hj Tapah <imuqri31@gmail.com>
2023-07-02 23:45:27 +0800

    Update verifyToken.js
    add verify admin

--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -8,7 +8,7 @@ const verifyToken = (req,res,next)=>{
             if(err) res.status(403).json("token is not valid!");
             req.user = user;
             next();
-        })
+        });
 
     }else{
         return res.status(401).json("you are not authenticated!")
@@ -18,11 +18,25 @@ const verifyToken = (req,res,next)=>{
 const verifyTokenAndAuthorization = ( req,res,next)=>{
     verifyToken(req,res,()=>{
         if(req.user.id === req.params.id || req.user.isAdmin){
-            next()
+            next();
         }else{
             res.status(403).json("you are not allowed to do that!")
         }
-    })
-}
+    });
+};
 
-module.exports = { verifyToken, verifyTokenAndAuthorization };
+const verifyTokenAndAdmin = ( req,res,next)=>{
+    verifyToken(req,res,()=>{
+        if(req.user.isAdmin){
+            next();
+        }else{
+            res.status(403).json("you are not allowed to do that!")
+        }
+    });
+};
+
+module.exports = { 
+    verifyToken, 
+    verifyTokenAndAuthorization,
+    verifyTokenAndAdmin,
+ };
